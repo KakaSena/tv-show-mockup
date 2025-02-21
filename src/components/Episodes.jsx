01fcafcc67dbfdec57566a5da51d4b5d6b7d4614
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-02-21 17:32:40 -0300

    episodes component

--- src/components/Episodes.jsx
+++ src/components/Episodes.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "../styles/Episodes.module.css";
 
 const Episodes = ({ episodes, onSelectEpisode }) => {
@@ -18,4 +19,25 @@ const Episodes = ({ episodes, onSelectEpisode }) => {
   );
 };
 
+Episodes.defaultProps = {
+  episodes: [
+    {
+      ID: "default-id",
+      Title: "No Title Available",
+      Synopsis: "No synopsis available.",
+    },
+  ],
+  onSelectEpisode: () => {}, // Default to a no-op function if no handler is provided
+};
+
+Episodes.propTypes = {
+  episodes: PropTypes.arrayOf(
+    PropTypes.shape({
+      ID: PropTypes.string.isRequired,
+      Title: PropTypes.string.isRequired,
+      Synopsis: PropTypes.string,
+    })
+  ),
+  onSelectEpisode: PropTypes.func,
+};
 export default Episodes;
