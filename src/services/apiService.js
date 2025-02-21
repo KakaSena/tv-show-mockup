e66dbdc498402fd2265a1ec4e4addec3c069132c
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-02-21 17:33:06 -0300

    apiservice fetching the show and the episodes

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://sample-api-78c77.firebaseio.com";
+const API_BASE_URL = "https://agile-releases.s3.us-east-1.amazonaws.com";
 
 export const fetchShow = async (showId) => {
-  const response = await axios.get(`${API_BASE_URL}/tv-shows/${showId}.json`);
+  const response = await axios.get(
+    `${API_BASE_URL}/tests/tv-shows/${showId}.json`
+  );
 
   return response.data;
 };
 
 export const fetchEpisodes = async (showId) => {
-  const response = await axios.get(`${API_BASE_URL}/episodes/${showId}.json`);
+  const response = await axios.get(
+    `${API_BASE_URL}/tests/episodes/${showId}.json`
+  );
   return response.data;
 };
