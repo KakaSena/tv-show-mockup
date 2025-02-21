84b757ad71c43975e18e7c27df2eea495079524c
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-02-21 16:49:49 -0300

    created baground with title, year, genre

---
+++ src/components/Background.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "../styles/Background.module.css";
+
+const Background = ({ title, genres, year, image }) => {
+  const Genres = genres.map((genre) => genre.Title).join(", ");
+
+  const backgroundImg = {
+    backgroundImage: image ? `url(${image})` : "none",
+  };
+
+  return (
+    <div className={styles.header} style={backgroundImg}>
+      <div className={styles.headerContent}>
+        <h1>{title}</h1>
+        <p>
+          {Genres} | {year}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+Background.defaultProps = {
+  title: "No Title Available",
+  genres: [],
+  year: "Year not specified",
+  image: "",
+};
+
+Background.propTypes = {
+  title: PropTypes.string,
+  genres: PropTypes.arrayOf(
+    PropTypes.shape({
+      ID: PropTypes.string,
+      Title: PropTypes.string,
+    })
+  ),
+  year: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  image: PropTypes.string,
+};
+
+export default Background;
