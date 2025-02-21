2592782aef53b982f929eb6ff04d08d398ad5caa
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-02-21 17:32:28 -0300

    episodes details components

--- src/components/EpisodeDetails.jsx
+++ src/components/EpisodeDetails.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "../styles/EpisodeDetails.module.css";
 
 const EpisodeDetails = ({ episode }) => {
@@ -6,11 +7,36 @@ const EpisodeDetails = ({ episode }) => {
 
   return (
     <div className={styles.episodeDetails}>
+      {episode.Image && (
+        <img
+          src={episode.Image}
+          alt={episode.Title}
+          className={styles.episodeImage}
+        />
+      )}
       <h2>{episode.Title}</h2>
       <p>{episode.Synopsis}</p>
       <p>Duration: {episode.Duration} minutes</p>
     </div>
   );
+};
+
+EpisodeDetails.defaultProps = {
+  episode: {
+    Title: "No Title Available",
+    Synopsis: "No synopsis available.",
+    Duration: "Duration not specified",
+    Image: "",
+  },
+};
+
+EpisodeDetails.propTypes = {
+  episode: PropTypes.shape({
+    Title: PropTypes.string,
+    Synopsis: PropTypes.string,
+    Duration: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    Image: PropTypes.string,
+  }),
 };
 
 export default EpisodeDetails;
