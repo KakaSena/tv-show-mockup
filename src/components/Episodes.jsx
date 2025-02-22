702c8eede208530d23cab346f55e87ff70eb3624
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-02-22 15:33:12 -0300

    episodes component

--- src/components/Episodes.jsx
+++ src/components/Episodes.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "../styles/Episodes.module.css";
+import EpisodeDetails from "./EpisodeDetails";
 
 const Episodes = ({ episodes, onSelectEpisode }) => {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
+  const handleEpisodeClick = (episode) => {
+    if (selectedEpisode?.ID === episode.ID) {
+      setSelectedEpisode(null);
+    } else {
+      setSelectedEpisode(episode);
+    }
+    onSelectEpisode(episode);
+  };
   return (
     <div className={styles.episodeList}>
       {episodes.map((episode) => (
         <div
           key={episode.ID}
-          className={styles.episode}
-          onClick={() => onSelectEpisode(episode)}
+          className={`${styles.episode} ${
+            selectedEpisode?.ID === episode.ID ? styles.selected : ""
+          }`}
+          onClick={() => handleEpisodeClick(episode)}
         >
-          <h3>{episode.Title}</h3>
-          <p>{episode.Synopsis}</p>
+          <div className={styles.episodeContent}>
+            <h3>
+              {episode.EpisodeNumber} {episode.Title}{" "}
+              <i className="bi bi-play-circle"></i>
+            </h3>
+          </div>
+
+          {selectedEpisode?.ID === episode.ID && (
+            <EpisodeDetails episode={selectedEpisode} />
+          )}
         </div>
       ))}
     </div>
