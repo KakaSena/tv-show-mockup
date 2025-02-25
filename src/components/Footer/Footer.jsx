3c0aacb938412f7d76a90ee3fe8be7b295230e4f
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-02-25 00:36:48 -0300

    late stuff

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,81 +1,10 @@
 import React, { useState } from "react";
 import styles from "./Footer.module.css";
-//import General from "../General/General";
-//import Cast from "../Cast/Cast";
 import { getFooterItems } from "../../config/footerConfig";
 import { useFooter } from "../../hooks/useFooter";
 
-// const Footer = ({ cast, show }) => {
-//   const [showGeneral, setShowGeneral] = useState(true);
-//   const [showCast, setShowCast] = useState(false);
-//   const [fadeOut, setFadeOut] = useState(false);
-
-//   const toggleGeneral = () => {
-//     if (showCast) {
-//       setFadeOut(true);
-//       setTimeout(() => {
-//         setShowCast(false);
-//         setShowGeneral(true);
-//         setFadeOut(false);
-//       }, 300);
-//     }
-//   };
-
-//   const toggleCast = () => {
-//     if (showGeneral) {
-//       setFadeOut(true);
-//       setTimeout(() => {
-//         setShowGeneral(false);
-//         setShowCast(true);
-//         setFadeOut(false);
-//       }, 300);
-//     }
-//   };
-
-//   return (
-//     <footer className={styles.footer}>
-//       <div className={styles.footerSection}>
-//         <button onClick={toggleGeneral} className={styles.GeneralButton}>
-//           GERAL
-//         </button>
-
-//         {showGeneral && (
-//           <div className={`${styles.content} ${fadeOut ? styles.fadeOut : ""}`}>
-//             <General show={show} />
-//           </div>
-//         )}
-//       </div>
-
-//       <div className={styles.footerSection}>
-//         <button onClick={toggleCast} className={styles.footerButton}>
-//           ELENCO
-//         </button>
-//         {showCast && (
-//           <div className={`${styles.content} ${fadeOut ? styles.fadeOut : ""}`}>
-//             <Cast cast={cast} />
-//           </div>
-//         )}
-//       </div>
-
-//       <div className={styles.footerSection}>
-//         <button className={styles.footerButton}>PRINCIPAIS PREMIOS</button>
-//         {showGeneral && (
-//           <div
-//             className={`${styles.content} ${fadeOut ? styles.fadeOut : ""}`}
-//           ></div>
-//         )}
-//       </div>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
-
 const Footer = ({ cast, show }) => {
-  // Get the footer items dynamically
   const footerItems = getFooterItems(show, cast);
-
-  // Use the custom hook to manage footer state and behavior
   const { activeItem, footerBarRef, footerItemsRef, handleItemClick } =
     useFooter(footerItems, "general");
 
@@ -103,4 +32,4 @@ const Footer = ({ cast, show }) => {
   );
 };
 
-export default Footer; // Default export
+export default Footer;
